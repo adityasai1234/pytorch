22c79039822b65359f42a4bcf58b5ac8feecf4f4
Frederik Gossen <frgossen@meta.com>
2026-03-24 14:32:47 -0700

    Accept GraphModule in create_compiler_config_extra
    ghstack-source-id: fe966af11b59b1c6492532633848c79193a622d0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/177852

--- torch/_inductor/compile_fx.py
+++ torch/_inductor/compile_fx.py
@@ -159,8 +159,6 @@ else:
     from torch._inductor.fb.utils import log_optimus_to_scuba, time_and_log
 
 if TYPE_CHECKING:
-    import types
-
     from torch._functorch._aot_autograd.schemas import (
         FQN,
         GraphInputName,
@@ -2277,8 +2275,12 @@ class CompilerConfigExtra:
 
 
 def create_compiler_config_extra(
-    config: types.ModuleType, gm_meta: dict[str, Any] | None = None
+    mod: GraphModule | torch._dynamo.utils.GmWrapper,
 ) -> CompilerConfigExtra:
+    gm = mod.gm if isinstance(mod, torch._dynamo.utils.GmWrapper) else mod
+    assert isinstance(gm, GraphModule)
+    gm_meta = gm.meta
+
     # Although cudagraphs may have been enabled via config, various
     # conditions (which are tested within the bowels of Inductor) may
     # force cudagraphs to be disabled.  This mutable box lets us retrieve
@@ -2783,8 +2785,7 @@ def _compile_fx_main(
 
         num_example_inputs = len(example_inputs_)
 
-        gm_meta = model_.meta if isinstance(model_, GraphModule) else None
-        compiler_config_extra = create_compiler_config_extra(config, gm_meta)
+        compiler_config_extra = create_compiler_config_extra(model_)
 
         decompositions = (
             decompositions if decompositions is not None else select_decomp_table()
